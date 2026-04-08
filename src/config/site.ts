0f1036f16ad2ae79ca3bd32909176dd369a50805
Mirumo <mysteryperson520@gmail.com>
2026-04-08 22:43:40 +0800

    feat: add gtm

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,3 +2,4 @@ export const POSTS_PER_PAGE = 10;
 export const TAGS_PER_PAGE = 20;
 export const SIDEBAR_TAG_COUNT = 10;
 export const RANDOM_PAGE_SIZE = 20;
+export const GOOGLE_TAG_MANAGER_ID = 'GTM-5KJ7LB34';
