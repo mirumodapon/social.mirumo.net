393cfba888193eb5df989374efa3ce66b83cb618
Mirumo <mysteryperson520@gmail.com>
2026-04-12 16:16:33 +0800

    feat: fix rss.xml

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,24 +4,25 @@ import type { APIContext } from 'astro';
 import { SITE_DESCRIPTION, SITE_NAME } from '../config/site';
 import { renderMarkdown } from '../utils/renderMarkdown';
 
-function hasBodyContent(body: string | undefined) {
-  return !!body?.trim();
-}
-
 export async function GET(context: APIContext) {
   const posts = await getCollection('posts');
   const items = await Promise.all(
     posts
-      .filter((post) => hasBodyContent(post.body))
       .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
-      .map(async (post) => ({
-        link: `/post/${post.id}/`,
-        description: await renderMarkdown(post.data.abstract),
-        content: await renderMarkdown(post.body ?? ''),
-        pubDate: new Date(post.data.date),
-        categories: post.data.tags,
-        customData: `<dc:creator>${post.data.author}</dc:creator>`,
-      })),
+      .map(async (post) => {
+        const url = `/post/${post.id}/`
+        const abstract = await renderMarkdown(post.data.abstract);
+        const content = `${abstract}\n\n${post.body?.trim() ? await renderMarkdown(post.body) : ''}\n\nRead more: ${url}#body`;
+
+        return {
+          link: url,
+          description: abstract,
+          content,
+          pubDate: new Date(post.data.date),
+          categories: post.data.tags,
+          customData: `<dc:creator>${post.data.author}</dc:creator>`,
+        }
+      })
   );
 
   return rss({
