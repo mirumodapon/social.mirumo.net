fc3fefb5e6bee5c06ad14d94c33d3db062ba7606
Mirumo <mysteryperson520@gmail.com>
2026-04-25 14:05:32 +0800

    chore: support subdirectory organization in posts without affecting URLs

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,11 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/posts' }),
+  loader: glob({
+    pattern: '**/*.md',
+    base: './src/content/posts',
+    generateId: ({ entry }) => entry.split('/').pop()!.replace(/\.md$/, ''),
+  }),
   schema: z.object({
     author: z.string(),
     handle: z.string(),
